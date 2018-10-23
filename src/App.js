3132203fbf85d4bfb6d9b441d8188207f1b52f93
zdc111 <zhangdaochun@apexsoft.com.cn>
2018-10-23 22:42:39 +0800

    章节10

--- src/App.js
+++ src/App.js
@@ -2,13 +2,40 @@ import React, { Component } from 'react';
 import './assets/css/App.css';
 import Home from './components/Home';
 import New from './components/New';
+import Home1 from './components/Home1';
+import Todolist from './components/Todolist';
+import Todolist01 from './components/Todolist01';
+import TodolistTest from './components/TodolistTest';
+import ReactFrom from './components/ReactFrom';
 class App extends Component {
     //render jsx
+  constructor(props){
+    super(props);
+    this.state={
+      view:false
+    }
+  }
+  changeView = ()=>{
+    let view = this.state.view;
+    this.setState({
+        view:!view
+    })
+  }
   render() {
+      const {view} = this.state.view;   /*在return中调用view 不能在{}内*/
     return (
       <div className="App">
+          <button onClick={this.changeView}>切换界面</button>
           {/*<Home/>*/}
-          <New/>
+          {/*<New/>*/}
+          {/*<Home1/>*/}
+         {/* <ReactFrom/>*/}
+          {/*<Todolist/>*/}
+          {
+              !this.state.view? <Todolist01/>:<TodolistTest/>
+          }
+
+          {/*<TodolistTest/>*/}
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -13,9 +13,42 @@ class Home extends Component{
                 userName:"道春",
             },
             title:"我是一个title",
-            color:'red'
+            color:'red',
+            msg:'我是一个msg',
+            message:'我是一个message',
+            something:'我是一个something'
         }
+        //初始化绑定
+        this.getSomething = this.getSomething.bind(this);
     }
+    run (){
+        alert('我是一个方法');
+    }
+    getData() {
+        alert(this.state.msg);
+    }
+    getMessage=()=> {
+        alert(this.state.message)
+    }
+    getSomething() {
+        alert(this.state.something)
+    }
+    setDate = ()=> {
+        this.setState({
+            msg: '这个改变后的message'
+
+        })
+        this.getData(); //第二次才改变
+    }
+    setMessage = (message)=> {
+        this.setState({
+            message: message
+
+        })
+        this.getMessage();
+
+    }
+
     render(){
         return (
             <div>
@@ -28,6 +61,20 @@ class Home extends Component{
                 <p>用户对象：{this.state.userInfo.userName}</p>
                 <label htmlFor="name">姓名</label>
                 <input id="name"/>
+                <hr/>
+                <button onClick={this.run}>执行方法</button>
+                <br/>
+                <button onClick={this.getData.bind(this)}>获取数据第一种:改变this指向的方法</button>
+                <br/>
+                <button onClick={this.getMessage}>获取数据第二种:箭头函数默认bind</button>
+                <br/>
+                <button onClick={this.getSomething}>获取数据第三种:初始化函数中进行bind</button>
+                <br/>
+                <button onClick={this.setDate}>设置state</button>
+                <br/>
+                <button onClick={this.setMessage.bind(this,'张三')}>执行方法传值</button>
+                <br/>
+                <button onClick={()=>this.setMessage('李四')}>执行方法传值箭头函数</button>
             </div>
         )
     }
